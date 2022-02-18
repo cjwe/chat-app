338457a3cd6016321783a1ddcb2903aca6add484
cjwe <christajwest@gmail.com>
2022-02-18 15:14:01 +0100

    Added gifted chat, state messages

--- components/Chat.js
+++ components/Chat.js
@@ -1,23 +1,80 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { GiftedChat, Bubble } from 'react-native-gifted-chat';
+import { View, Platform, KeyboardAvoidingView } from 'react-native';
 
 export default class Chat extends React.Component {
-  render() {
-    let name = this.props.route.params.name;
-    this.props.navigation.setOptions({ title: name });
+  constructor() {
+    super();
+    this.state = {
+      messages: [],
+    };
+  }
 
+  componentDidMount() {
+    let { name } = this.props.route.params;
+
+    this.setState({
+      messages: [
+        {
+          _id: 1,
+          text: 'Hello, developer!',
+          createdAt: new Date(),
+          user: {
+            _id: 2,
+            name: 'React Native',
+            avatar: 'https://placeimg.com/140/140/any',
+          },
+        },
+        {
+          _id: 2,
+          text: `${name} has entered the chat`,
+          createdAt: new Date(),
+          system: true,
+        },
+      ],
+    });
+  }
+
+  onSend(messages = []) {
+    this.setState((previousState) => ({
+      messages: GiftedChat.append(previousState.messages, messages),
+    }));
+  }
+
+  renderBubble(props) {
+    return (
+      <Bubble
+        {...props}
+        wrapperStyle={{
+          right: {
+            backgroundColor: '#000',
+          },
+        }}
+      />
+    );
+  }
+
+  render() {
     const { bgColor } = this.props.route.params;
 
     return (
       <View
         style={{
           flex: 1,
-          justifyContent: 'center',
-          alignItems: 'center',
           backgroundColor: bgColor,
         }}
       >
-        <Text>Chat Screen</Text>
+        <GiftedChat
+          renderBubble={this.renderBubble.bind(this)}
+          messages={this.state.messages}
+          onSend={(messages) => this.onSend(messages)}
+          user={{
+            _id: 1,
+          }}
+        />
+        {Platform.OS === 'android' ? (
+          <KeyboardAvoidingView behavior="height" />
+        ) : null}
       </View>
     );
   }
